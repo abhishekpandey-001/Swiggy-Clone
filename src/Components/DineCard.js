0f9c2cf4c37a1deb96d8ccc4e61b64353a7f8101
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-09-25 18:44:17 +0530

    removed tester file

--- src/Components/DineCard.js
+++ src/Components/DineCard.js
@@ -1,8 +1,12 @@
 
 export default function DineCard({DineData}){
     return(
-        <div className="w-[80%] container mx-auto mt-10 flex">
-        <img className="w-[100%]" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+DineData?.info?.mediaFiles[0]?.url}></img>
+        <div className="max-w-sm flex-none">
+            <div className="relative">
+                <img className="w-80 h-50 object-cover" src={"https://media-assets.swiggy.com/swiggy/image/upload/"+DineData?.info?.mediaFiles[0]?.url}></img>
+                <p className="absolute bottom-2 left-2 text-xl text-white">{DineData?.info?.name}</p>
+                <p className="absolute bottom-2 right-2 text-xl text-white">{DineData?.info?.rating?.value}</p>
+            </div>
         </div>
     )
 }
--- src/Components/DineOption.js
+++ src/Components/DineOption.js
@@ -1,12 +1,25 @@
-import { dineoutRestaurants } from "../Utils/DinData"
-import DineCard from "./DineCard"
+import { dineoutRestaurants } from "../Utils/DinData";
+import DineCard from "./DineCard";
 
-export default function DineOption(){
-    return(
-        <>
-        {
-            dineoutRestaurants.map((DineData)=><DineCard key={DineData?.info?.id} DineData={DineData}></DineCard>)
-        }
-        </>
-    )
+export default function DineOption() {
+  return (
+    <>
+      <p className="text-black font-bold text-2xl mt-10 ml-40">
+        Discover best restaurants on Dineout
+      </p>
+      <div className="w-[80%] container mx-auto overflow-x-auto mb-20">
+        <div className="flex flex-nowrap gap-3 mt-10">
+          <div className="grid grid-rows-1 auto-cols-max grid-flow-col gap-6">
+            {dineoutRestaurants.map((DineData) => (
+              <DineCard key={DineData?.info?.id} DineData={DineData} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+// {
+//     dineoutRestaurants.map((DineData)=><DineCard key={DineData?.info?.id} DineData={DineData}></DineCard>)
+// }
