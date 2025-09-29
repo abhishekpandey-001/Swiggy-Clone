bb69f1be28b8b83dc198a67e651bda063e530bce
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-09-29 19:41:50 +0530

    Added veg/non-veg filteration

--- src/Components/MenuCard.js
+++ src/Components/MenuCard.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import RestInfo from "./RestInfo"
 
-export default function MenuCard({menuitems}){
+export default function MenuCard({menuitems, foodselected}){
 
     const [isOpen, setIsOpen] = useState(true)
 
@@ -12,7 +12,7 @@ export default function MenuCard({menuitems}){
             <div className="w-full">
                 <p className="text-3xl font-bold mb-4">{menuitems?.title}</p>
                 <div>
-                    {menuitems?.categories?.map((items)=><MenuCard key={items?.title} menuitems={items}></MenuCard>)}
+                    {menuitems?.categories?.map((items)=><MenuCard key={items?.title} menuitems={items} foodselected={foodselected}></MenuCard>)}
                 </div>
             </div>
         )
@@ -31,6 +31,41 @@ export default function MenuCard({menuitems}){
         )
     }
 
+
+    if(foodselected==='veg'){
+        return(
+        <div className="w-full">
+            <div className="flex justify-between ">
+            <p className="text-2xl font-bold">{menuitems.title}</p>
+            <button className="text-xl mr-20" onClick={()=>setIsOpen(!isOpen)}>{isOpen?"▲":"▼"}</button>
+            </div>
+            <div>
+                {
+                    menuitems?.itemCards?.filter((food)=>"isVeg" in food?.card?.info).map((items)=><RestInfo key={items?.card?.info?.id} restData={items?.card?.info}></RestInfo>)
+                }
+            </div>
+            <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
+        </div>
+        )
+    }
+
+
+    if(foodselected==='nonveg'){
+      return(
+        <div className="w-full">
+            <div className="flex justify-between ">
+            <p className="text-2xl font-bold">{menuitems.title}</p>
+            <button className="text-xl mr-20" onClick={()=>setIsOpen(!isOpen)}>{isOpen?"▲":"▼"}</button>
+            </div>
+            <div>
+                {
+                    menuitems?.itemCards?.filter((food)=>!("isVeg" in food?.card?.info)).map((items)=><RestInfo key={items?.card?.info?.id} restData={items?.card?.info}></RestInfo>)
+                }
+            </div>
+            <div className="h-5 bg-gray-200 mt-2 mb-2"></div>
+        </div>
+        )
+    }
 
 
     return(
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -6,6 +6,7 @@ import MenuCard from "./MenuCard";
 export default function RestaurantMenu(){
    
     let {id} = useParams();
+    const [selected, setSelected] = useState(null)
     console.log(id);
 
     const [RestData, setRestData] = useState([]);
@@ -29,11 +30,21 @@ export default function RestaurantMenu(){
        console.log(RestData);
 
     return(
+      <div>
+
+
+        <div className="w-[80%] mt-20 mb-20 mx-auto">
+              <button className={`tex-2xl py-2 px-8 mr-4 border rounded-2xl ${selected=="veg"?"bg-emerald-400":"bg-gray-400"}`} onClick={()=>setSelected(selected==='veg'? null:'veg')}>Veg</button>
+              <button className={`tex-2xl py-2 px-8 border rounded-2xl ${selected==="nonveg"?"bg-red-600":"bg-gray-400"}`} onClick={()=>setSelected(selected==="nonveg"?null:"nonveg")}>Non-Veg</button>
+        </div>
+
         <div className="w-[80%] mx-auto mt-20">
           {
-            RestData.map((menuitems)=><MenuCard key={menuitems?.card?.card.title} menuitems={menuitems?.card?.card}></MenuCard>)
+            RestData.map((menuitems)=><MenuCard key={menuitems?.card?.card.title} menuitems={menuitems?.card?.card} foodselected ={selected}></MenuCard>)
           }
         </div>
+
+      </div>
     )
 
 }
