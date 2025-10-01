7f5fc8f27d2f4099640a2b367e41092894e6fd02
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-10-01 08:10:53 +0530

    created add functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Restaurant from "./Components/Restaurant";
 import { BrowserRouter, Routes, Route } from "react-router";
 import RestaurantMenu from "./Components/RestaurantMenu";
 import SearchFood from "./Components/SearchFood";
+import RestHeader from "./Components/RestHeader";
+import SecondaryHome from "./Components/SecondaryHome";
 
 function App(){
     return(
@@ -12,9 +14,11 @@ function App(){
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home></Home>}></Route>
-            <Route path="/restaurant" element={<Restaurant></Restaurant>}></Route>
-            <Route path="/city/delhi/:id" element={<RestaurantMenu></RestaurantMenu>}></Route>
-            <Route path="/city/delhi/:id/search" element={<SearchFood></SearchFood>}></Route>
+            <Route element={<SecondaryHome/>}>
+              <Route path="/restaurant" element={<Restaurant></Restaurant>}></Route>
+              <Route path="/city/delhi/:id" element={<RestaurantMenu></RestaurantMenu>}></Route>
+              <Route path="/city/delhi/:id/search" element={<SearchFood></SearchFood>}></Route>
+            </Route>
         </Routes>
         </BrowserRouter>
         </>
